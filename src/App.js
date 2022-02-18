7159d973eb5f452f7c3b90b60715edf9d98ce654
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-02-18 21:35:15 +0530

    added api key

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-	const [coins,setCoins] = useState([]);
+	const [coins, setCoins] = useState([]);
+	const url =
+		"https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+	useEffect(() => {
+		axios
+			.get(url)
+			.then((response) => {
+				setCoins(response.data);
+				console.log(response.data[0]);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 	return (
 		<>
-			<h1>Cryptsearch</h1>
+			<h1>hi</h1>
 		</>
 	);
 };
